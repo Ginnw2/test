123ae221a273552f913319d57affc98806ff6312
Ginnw@ <dev@vchechnev.ru>
2019-03-06 15:45:35 +0300

    Slick

--- test.php
+++ test.php
@@ -17,6 +17,8 @@
     <div>5</div>
     <div>6</div>
 </div>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script>
     $('.single-item').slick();
