8ac7076b40d54239dffdf77821448078c1d99415
Ginnw@ <dev@vchechnev.ru>
2019-03-14 15:14:42 +0300

    Slick

--- test.php
+++ test.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" href="http://kenwheeler.github.io/slick/slick/slick-theme.css">
     <title>Slick slider</title>
 </head>
 <body>
@@ -90,7 +91,7 @@
 
     $('.multiple-items').on('afterChange', function(event, slick, currentSlide) {
         console.log(slick['slideCount']);
-        if (currentSlide == 0) {
+        if (currentSlide == 0 && slick['slideCount']==5) {
             var id = 1;
 
             var form_data = new FormData();
