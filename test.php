3e62f2e335b0a0308adc54dcdae2eed2168841b7
Ginnw@ <dev@vchechnev.ru>
2019-06-21 14:24:51 +0300

    test upload catalog

--- test.php
+++ test.php
@@ -67,7 +67,7 @@
     $result = curl_exec($ch);
     curl_close ($ch);
     $resCatalogTree = json_decode($result);
-    echo "<pre>"; print_r($resCatalogTree);
+    echo "<pre>"; print_r($resCatalogTree[0]);
     //Список товаров получаем аналогичным образом
     //Получение всех товаров в наличии их цены
     $ch = curl_init("https://b2b.i-t-p.pro/api");
